42ca6e54e06bc33f8d11cce148273d8b1e799641
ptizrootofevil <vvts.00@mail.ru>
2025-06-20 17:07:01 +0300

    v1.0

--- Life/Components/Pages/Game_life.razor.cs
+++ Life/Components/Pages/Game_life.razor.cs
@@ -1,3 +1,6 @@
+using System.Xml.Schema;
+using System.Timers;
+
 namespace Life.Components.Pages
 {
     public partial class Game_life
@@ -8,6 +11,10 @@ namespace Life.Components.Pages
         private HashSet<Cell> livingCells = new HashSet<Cell>();
         private HashSet<Cell> potentialCells = new HashSet<Cell>();
         private bool initialized = false;
+        private int generation = 0;
+        private System.Timers.Timer timer;
+        private bool isRunning = false;
+        private string lastTickTime;
 
         protected override void OnInitialized()
         {
@@ -20,6 +27,18 @@ namespace Life.Components.Pages
                 }
             }
             initialized = true;
+            timer = new System.Timers.Timer(1000); // 2 second interval
+            timer.Elapsed += OnTimerTick;
+            timer.AutoReset = true;
+        }
+         private async void OnTimerTick(object sender, ElapsedEventArgs e)
+        {
+            await InvokeAsync(() => 
+            {
+                Tick(); // Your tick function
+                lastTickTime = DateTime.Now.ToString("HH:mm:ss.fff");
+                StateHasChanged(); // Update UI
+            });
         }
         private void ChangeCell(int row, int col)
         {
@@ -28,13 +47,27 @@ namespace Life.Components.Pages
                 if (row >= 0 && row < height && col >= 0 && col < width)
                 {
                     grid[row, col].IsAlive = !grid[row, col].IsAlive;
-                    if (!livingCells.Remove(grid[row, col]) && grid[row, col].IsAlive) livingCells.Add(grid[row, col]);
+                    if (grid[row, col].IsAlive) livingCells.Add(grid[row, col]);
+                    else livingCells.Remove(grid[row, col]);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error toggling value: {ex.Message}");
             }
+        }
+
+        private void ToggleTimer()
+        {
+            if (isRunning)
+            {
+                timer.Stop();
+            }
+            else
+            {
+                timer.Start();
+            }
+            isRunning = !isRunning;
         }
 
         class Cell
@@ -60,29 +93,46 @@ namespace Life.Components.Pages
             {
                 foreach (Cell cell in livingCells)
                 {
-                    this.grid[cell.X - 1, cell.Y - 1].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X - 1, cell.Y - 1]);
-
-                    this.grid[cell.X - 1, cell.Y].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X - 1, cell.Y]);
-
-                    this.grid[cell.X - 1, cell.Y + 1].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X - 1, cell.Y + 1]);
-
-                    this.grid[cell.X, cell.Y - 1].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X, cell.Y - 1]);
-
-                    this.grid[cell.X, cell.Y + 1].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X, cell.Y + 1]);
-
-                    this.grid[cell.X + 1, cell.Y - 1].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X - 1, cell.Y - 1]);
-
-                    this.grid[cell.X + 1, cell.Y].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X - 1, cell.Y]);
-
-                    this.grid[cell.X + 1, cell.Y].LivingNeighbours += 1;
-                    this.potentialCells.Add(this.grid[cell.X - 1, cell.Y + 1]);
+                    if (cell.X > 0 && cell.Y > 0)
+                    {
+                        this.grid[cell.X - 1, cell.Y - 1].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X - 1, cell.Y - 1]);
+                    }
+                    if (cell.X > 0)
+                    {
+                        this.grid[cell.X - 1, cell.Y].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X - 1, cell.Y]);
+                    }
+                    if (cell.X > 0 && cell.Y + 1 < width)
+                    {
+                        this.grid[cell.X - 1, cell.Y + 1].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X - 1, cell.Y + 1]);
+                    }
+                    if (cell.Y > 0)
+                    {
+                        this.grid[cell.X, cell.Y - 1].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X, cell.Y - 1]);
+                    }
+                    if (cell.Y + 1 < width)
+                    {
+                        this.grid[cell.X, cell.Y + 1].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X, cell.Y + 1]);
+                    }
+                    if (cell.X + 1 < height && cell.Y > 0)
+                    {
+                        this.grid[cell.X + 1, cell.Y - 1].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X + 1, cell.Y - 1]);
+                    }
+                    if (cell.X + 1 < height)
+                    {
+                        this.grid[cell.X + 1, cell.Y].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X + 1, cell.Y]);
+                    }
+                    if (cell.X + 1 < height && cell.Y + 1 < width)
+                    {
+                        this.grid[cell.X + 1, cell.Y + 1].LivingNeighbours += 1;
+                        this.potentialCells.Add(this.grid[cell.X + 1, cell.Y + 1]);
+                    }
 
                 }
 
@@ -94,7 +144,7 @@ namespace Life.Components.Pages
                         cell.IsAlive = true;
                         this.livingCells.Add(cell);
                     }
-                    else if (cell.LivingNeighbours < 1 || cell.LivingNeighbours > 3)
+                    else if (cell.LivingNeighbours < 2 || cell.LivingNeighbours > 3)
                     {
                         cell.IsAlive = false;
                         this.livingCells.Remove(cell);
@@ -102,6 +152,7 @@ namespace Life.Components.Pages
                     cell.LivingNeighbours = 0;
                 }
                 potentialCells.Clear();
+                this.generation += 1;
             }
         }
     }
