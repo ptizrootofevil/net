cc7d75645e29dc45f46afade56e9681c6c1e6761
ptizrootofevil <vvts.00@mail.ru>
2025-06-22 12:57:22 +0300

    v1.1.1

--- Life/obj/Debug/net9.0/Life.AssemblyInfo.cs
+++ Life/obj/Debug/net9.0/Life.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Life")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ca6e54e06bc33f8d11cce148273d8b1e799641")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ace7f516f4102a45995177f598a1a8433904860")]
 [assembly: System.Reflection.AssemblyProductAttribute("Life")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Life")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
